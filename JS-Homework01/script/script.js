a3b2d3e9a04e21b19c3b809145534adb100fd7ec
Ilija Pecevski <49444973+Pecevski@users.noreply.github.com>
2020-05-10 01:53:50 +0200

    Add files via upload

---
+++ JS-Homework01/script/script.js
@@ -0,0 +1,18 @@
+// Calculate area of Circle
+
+const pi = Math.PI;
+let circleRadius = 10;
+let areaOfCircle = pi * (circleRadius * circleRadius);
+
+alert(areaOfCircle);
+
+
+// Calculate the price of  Phones
+
+let numberOfPhones = 30;
+let taxRate = 0.05;
+let priceBeforeTax = 119.95;
+let priceWithTax = priceBeforeTax + (priceBeforeTax * taxRate);
+let totalPriceOfPhones = numberOfPhones * priceWithTax;
+
+alert(totalPriceOfPhones);
